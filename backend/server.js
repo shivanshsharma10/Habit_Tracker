c28d40f33bcb0336733d31b72015de81d6ae3c95
shivanshsharma10 <outcastedstory@gmail.com>
2025-12-11 05:17:58 +0530

    tried

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use('/api/habits', habitRoutes);
+
 // Connect to Database
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
